e393a301f02d35bff781de433b34d5cce7d1ecff
jackreborn <15683842812@163.com>
2024-06-26 09:52:39 +0800

    extract wordbook

---
+++ docs/exract_wordbook.py
@@ -0,0 +1,38 @@
+import os
+import re
+import json
+
+base_dir = "./_posts"
+
+wordbook = {
+    "words": []
+}
+
+
+
+def scan_dir(dir):
+    for sub_dir_name in os.listdir(dir):
+        sub_dir = f'{dir}/{sub_dir_name}'
+        if os.path.isdir(sub_dir):
+            scan_dir(sub_dir)
+        else:
+           if sub_dir.endswith(".md"):
+               with open(sub_dir, "r", encoding="utf-8") as file:
+                   for line in file.readlines():
+                       words_prefix = "Words: "
+                       if line.startswith(words_prefix):
+                        content = line[len(words_prefix):]
+                        regex = re.compile(r"([\w]+)(\(.+?\)){0,1}")
+                        for match in regex.finditer(content):
+                            word = {
+                                "word":  match.group(1),
+                                "hint": match.group(2)
+                            }
+                            wordbook["words"].append(word)
+
+
+if __name__ == "__main__":
+    scan_dir(base_dir)
+
+    with open("assets/wordbook.json", "w", encoding="utf-8") as file:
+        json.dump(wordbook, file)
